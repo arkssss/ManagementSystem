da891bb692756d2aa2a217f51d5fdd9088aafe3d
Ark <522500442@qq.com>
2018-06-05 09:59:19 +0800

    基于PHP的仪器检定管理系统

---
+++ VerSystem/Common/Conf/config.php
@@ -0,0 +1,30 @@
+<?php
+return array(
+    //'配置项'=>'配置值'
+
+    //默认加载模块
+    // 'DEFAULT_MODULE' => 'Admin' ,
+
+
+    //数据库链接操作
+//    'DB_TYPE' => 'mysql',
+//    'DB_HOST' => 'localhost',
+//    'DB_USER' => 'root',
+//    'DB_PWD'  => '19951103',
+//    'DB_NAME' => 'thinkphp',
+//    'DB_PORT' => '3306',
+//    'DB_PREFIX'=>'think_',
+
+    //PDO专用定义
+    'DB_TYPE' => 'pdo',
+    'DB_USER' => 'root',
+    'DB_PWD'  => '19951103',
+    'DB_PREFIX'=>'think_',
+    'DB_DSN'  => 'mysql:host=localhost;dbname=versystem;charset=UTF8',
+
+    //页面调试工具
+    'SHOW_PAGE_TRACE' => true,
+
+
+);
+
